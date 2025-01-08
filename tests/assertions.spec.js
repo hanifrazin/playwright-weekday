0c29d5c742f7ae6fd6d3b69be20567e834b00a19
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2025-01-08 15:26:52 +0700

    rename variable item

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -15,15 +15,15 @@ test('assertion dan locator', async ({ page }) => {
     await expect(btnLogin).toContainText('Login')
     await btnLogin.click()
 
-    const item1 = page.locator('#item_4_title_link > div')
-    await expect(item1).toContainText('Sauce Labs Backpack')
-    const pilihItem1 = page.locator('#add-to-cart-sauce-labs-backpack')
-    await pilihItem1.click()
+    const itemBackpack = page.locator('#item_4_title_link > div')
+    await expect(itemBackpack).toContainText('Sauce Labs Backpack')
+    const pilihBackpack = page.locator('#add-to-cart-sauce-labs-backpack')
+    await pilihBackpack.click()
 
-    const item2 = page.locator('#item_1_title_link > div')
-    await expect(item2).toContainText('Sauce Labs Bolt T-Shirt')
-    const pilihItem2 = page.locator('#add-to-cart-sauce-labs-bolt-t-shirt')
-    await pilihItem2.click()
+    const itemTShirt = page.locator('#item_1_title_link > div')
+    await expect(itemTShirt).toContainText('Sauce Labs Bolt T-Shirt')
+    const pilihBoltTShirt = page.locator('#add-to-cart-sauce-labs-bolt-t-shirt')
+    await pilihBoltTShirt.click()
 
     const cart = page.locator('#shopping_cart_container > a')
     await cart.click()
